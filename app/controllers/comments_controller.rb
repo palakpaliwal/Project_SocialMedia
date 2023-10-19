2e35e3676f77045751b26ad600995923ade6faa0
Palak <palak@gmail.com>
2023-10-19 18:50:43 +0530

    msg

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -4,29 +4,31 @@ class CommentsController < ApplicationController
     def index
       @comments = Comment.all
       @users = User.all
+      @post = Post.find(params[:post_id])
     end
 
     def create
+        debugger
         @post = Post.find(params[:post_id])
         @comment = @post.comments.create(comment_params)
-        @comment.user_id = current_user.id
+        @comment.user = current_user                                                                     
         @comment.save
         redirect_to profile_index_path
     end
 
     def show
-       debugger
         @post = Post.find(params[:post_id])
     end
 
 
     def destroy
-        @comment = current_user.comments.find(params[:id])
-        if @comment.present?
-            @comment.destroy
+      @comments = current_user.comments.find(params[:id])
+      @comments.each do|comment|
+        if comment.present?
+            comment.destroy
             redirect_to root_path, notice: ' comment deleted'
         end
-
+      end
     end
    
     def comment_button
